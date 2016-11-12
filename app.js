e1e0fd303bcb0e0e6a151e213b2f938a73b98614
Justin Etzine <justetz@gmail.com>
2016-11-12 17:20:38 -0500

    Converted to ES6 patterns

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 'use strict';
 
 // Dependencies
-var express = require('express');
-var credentials = require('./secrets.json');
-var bodyParser = require('body-parser');
-var twilio = require('twilio');
-var cfenv = require('cfenv');
+const express = require('express')
+const credentials = require('./secrets.json')
+const bodyParser = require('body-parser')
+const twilio = require('twilio')
+const cfenv = require('cfenv')
 
 // Initialize Twilio
-var client = twilio(credentials.twilio.account_sid, credentials.twilio.auth_token)
+let client = twilio(credentials.twilio.account_sid, credentials.twilio.auth_token)
 
 // create a new express server
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+let app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`))
 
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+const appEnv = cfenv.getAppEnv()
 
 function sendMessage(target, message) {
     client.sendMessage({
         to: target,
         from: '+xxx',
         body: message
-    });
+    })
 }
 
-app.post('/incoming', function (req, res) {
+app.post('/incoming', (req, res) => {
   console.log(req.body);
 })
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-    // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
-});
+// print a message when the server starts listening
+app.listen(appEnv.port, '0.0.0.0', () => console.log(`server starting on ${appEnv.url}`));
